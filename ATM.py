78a3c863b61733606a1c9ebb6bc23a0d615de951
OralRobert <oralrobert96@gmail.com>
2024-05-16 22:30:49 +0530

    'learn'

---
+++ ATM.py
@@ -0,0 +1,22 @@
+amount = 4000
+
+if amount%50==0:
+    if amount >= 500:
+        print(amount)
+        notes = amount//500 
+        print("500 rs notes are",notes)
+        amount = amount %500
+    if amount >= 200:
+        notes = amount//200
+        print("200 rs notes are",notes)
+        amount = amount%200
+    if amount >= 100:
+        notes = amount//100
+        print("100 rs notes are",notes)
+        amount = amount%100
+    if amount >= 50:
+        notes = amount//50
+        print("50 rs notes are",notes)
+    
+else:
+    print("Amount should be multiple of 50")
---
+++ user_ input.py
@@ -0,0 +1,14 @@
+a = input("enter yoyr name : ")
+b = int(input("enter yopur age : "))
+print(type(a),type(b))
+
+a= 1.2
+print(type(int(a)))
+print(int(a))
+
+c=2+3j
+print(type(c))
+
+ab = 11
+print(float(ab))
+
