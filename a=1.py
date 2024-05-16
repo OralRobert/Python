d11bfa8d79ae53ef89359c298535c7902441d15d
OralRobert <oralrobert96@gmail.com>
2024-05-16 13:26:06 +0530

    'learn'

---
+++ a=1.py
@@ -0,0 +1,9 @@
+a = 1
+print(id(a))
+b=1
+print(b)
+b=12
+print(b)
+print(id(b))
+print('''Hi TUSHAR 
+my name ROBERT''')
---
+++ print star.py
@@ -0,0 +1,18 @@
+print("*"*20)
+print("#",end=" ")
+print("#",end=" ")
+print("#")
+
+print("hello guys","how are you","where are you from",sep="--")
+print(11&8)
+print(20^30)
+print(30&40)
+print(10&7)
+print(10|7)
+print(~10)
+print(~-10)
+print(10>>1)
+print(10>>2)
+print(10<<1)
+print(10<<2)
+
