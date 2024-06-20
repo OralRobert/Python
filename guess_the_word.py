e1f0d9340ab733c65b3dfba50f183c1987efef87
OralRobert <oralrobert96@gmail.com>
2024-06-20 11:40:57 +0530

    'learn

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,33 +1,61 @@
-import random
-words = ['robert','berort','mishraji','mijirash','rajit','arjit',
-         'krushna','rushank','tanaya','yatana','dhashra','namita','manita',
-         'netra','terna','ashish','shisha','abhishek','vishal','shalvi']
-score = 0
-while True:
-    cwords = list(random.choice(words))
-    print("your given word","".join(cwords))
-    user_word = input("enter your answer : ")
-    if user_word == "quit":
-        break
+##import random
+##words = ['robert','berort','mishraji','mijirash','rajit','arjit',
+##         'krushna','rushank','tanaya','yatana','dhashra','namita','manita',
+##         'netra','terna','ashish','shisha','abhishek','vishal','shalvi']
+##score = 0
+##while True:
+##    cwords = list(random.choice(words))
+##    print("your given word","".join(cwords))
+##    user_word = input("enter your answer : ")
+##    if user_word == "quit":
+##        break
+##
+##    for letter in user_word:
+##        if letter in cwords:
+##            cwords.remove(letter)
+##            valid = True
+##
+##        else:
+##            valid = False
+##            
+##
+##    if valid and user_word in words:
+##        score = score + len(user_word)
+##        print(f"correct your score is {score}")
+##    else:
+##        print("your answer is wrong")
+##print("Thank you so much")
 
-    for letter in user_word:
-        if letter in cwords:
-            cwords.remove(letter)
-            valid = True
 
-        else:
-            valid = False
-            
+letters = [['h','o','l','i','d','a','y'],
+           ['p','r','o','g','r','a','m','m','i','n','g'],
+           ['b','o','o','t','c','a','m','p'],
+           ['f','l','o','w','c','h','a','r','t'],
+           ['w','o','r','d','s','c','a','p','e','s']]
+words = [["hi","hay","day","had","lay","dal","lad","lid","hold","lady","hail"],
+         ["go","an","in","no","on","map","mom","gap","gag","pig","man","ping",
+          "pong","pram","prom","ramp"],
+         ["am","at","to","cab","cap","cob","cop","map","mop","act","bat","camp",
+          "comb","boom","pact","atom"],
+         ["of","at","or","to","caw","cow","how","who","calf","claw","flaw","flow",
+          "fowl","wolf","crow","half"],
+         ["we","do","as","cap","caw","cop","cow","paw","cod","dew","pad","cape",
+          "cope","crap","crew","crop"]];
 
-    if valid and user_word in words:
-        score = score + len(user_word)
-        print(f"correct your score is {score}")
-    else:
-        print("your answer is wrong")
-print("Thank you so much")
+lives = 5;
+level = 0;
+score = 0;
 
+while level<5:
+    print('Level',level+1) #('Level {}'.format(level+1))
+    print('Create 3 words using the given letters')
+    break
 
-        
-
+    for c in letters[level]:
+        continue
+        print('{}\t'.format(c),end='');
+    print()
+    
+    
     
     
--- task.py
+++ task.py
@@ -18,3 +18,36 @@ print(a*5)
 a = "hello"
 b = "world"
 print(a+b)
+
+#Print your name 10 times without using loop and manually
+a = " Robert "
+print(a*10)
+
+#WAP for Factorial 
+n = 5
+f = 1
+
+for i in range(1,n+1):
+    f*=i
+
+print(f)
+
+#Wap for factorial using recurssion
+def f(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * f(n-1)
+
+n = 5
+print(f(n))
+
+# Find pow of n using recurssion
+def pow(n,p):
+    if p==0:
+        return 1
+    else:
+        return pow(n,p-1)
+
+print(pow(2,3))
+
